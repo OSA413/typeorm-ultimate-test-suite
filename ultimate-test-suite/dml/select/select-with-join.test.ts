c852af8a4bf447475269e1cbeb00bbae4a56bb17
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2025-12-23 06:58:14 +0500

    Revert "also add .concurrent to test function"
    This reverts commit e21471737a8ebb68b22a7f7a733d7b1e08e52592.

--- ultimate-test-suite/dml/select/select-with-join.test.ts
+++ ultimate-test-suite/dml/select/select-with-join.test.ts
@@ -45,7 +45,7 @@ describe("Ultimate Test Suite > DML > Select (Joins)", () => {
     afterAll(() => closeTestingConnections(dataSources))
 
     generateTests().map(testCase => {
-        test.concurrent(testCase.metadata.entity.entity.name+": "+JSON.stringify(testCase.find), async () => {
+        test(testCase.metadata.entity.entity.name+": "+JSON.stringify(testCase.find), async () => {
             await Promise.all(dataSources.map(async dataSource => {
                 const repo = dataSource.getRepository(testCase.metadata.entity.entity);
                 const findResult = await repo.find({
